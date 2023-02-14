70e7d4c406ba52260e5d3767867b7cf39f15d484
hitesh <kumarhk1002@gmail.com>
2023-02-14 11:14:40 -0800

    uploading build

--- backend/server.js
+++ backend/server.js
@@ -51,7 +51,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "https://massenger-frontend.vercel.app",
+    origin: "https://massenger-frontend.vercel.app/",
   },
 });
 io.on("connection", (socket) => {
